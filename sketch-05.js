52ec5df26379c76baa6b8315451024d29f16f0ad
0xleodev <0xleodev@proton.me>
2023-09-22 13:05:26 +0200

    centered text variable

--- sketch-05.js
+++ sketch-05.js
@@ -4,91 +4,40 @@ const settings = {
   dimensions: [1080, 1080],
 };
 
+let text = "A";
+let fontSize = 1200;
+let fontFamily = "serif";
+
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = "#F5F5F5";
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    let numOfRec = 7; // 1-14
-    let numOfGaps = numOfRec + 1;
+    context.fillStyle = "black";
+    context.font = `${fontSize}px ${fontFamily}`;
+    context.textBaseline = "top";
+    // context.textAlign = "center";
 
-    let gapSize = 20;
-    let rectSize = (width - gapSize * numOfGaps) / numOfRec;
+    const metrics = context.measureText(text);
+    const mx = metrics.actualBoundingBoxLeft * -1;
+    const my = metrics.actualBoundingBoxAscent * -1;
 
-    let randomiser = 0.3;
+    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const mh =
+      metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 
-    let droveRec = (x, y, size, color) => {
-      context.lineWidth = width * 0.005;
-      context.fillStyle = color;
-      context.beginPath();
-      context.fillRect(x, y, size, size);
-      context.rect(x, y, size, size);
-      context.stroke();
-    };
+    const x = (width - mw) * 0.5 - mx;
+    const y = (height - mh) * 0.5 - my;
 
-    for (let i = 0; i < numOfRec; i++) {
-      for (let j = 0; j < numOfRec; j++) {
-        let x = gapSize + (rectSize + gapSize) * i;
-        let y = gapSize + (rectSize + gapSize) * j;
-        let color = "#edf8e9";
-        droveRec(x, y, rectSize, color);
+    context.save();
+    context.translate(x, y);
 
-        let randomise = Math.random();
-        if (randomise > randomiser) {
-          let color = "#bae4b3";
-          droveRec(
-            x + rectSize * 0.1,
-            y + rectSize * 0.1,
-            rectSize - rectSize * 0.2,
-            color
-          );
+    context.beginPath();
+    context.rect(mx, my, mw, mh);
+    context.stroke();
 
-          let randomise = Math.random();
-          if (randomise > randomiser) {
-            let color = "#74c476";
-            droveRec(
-              x + rectSize * 0.2,
-              y + rectSize * 0.2,
-              rectSize - rectSize * 0.4,
-              color
-            );
-
-            let randomise = Math.random();
-            if (randomise > randomiser) {
-              let color = "#31a354";
-              droveRec(
-                x + rectSize * 0.3,
-                y + rectSize * 0.3,
-                rectSize - rectSize * 0.6,
-                color
-              );
-
-              let randomise = Math.random();
-              if (randomise > randomiser) {
-                let color = "#006d2c";
-                droveRec(
-                  x + rectSize * 0.4,
-                  y + rectSize * 0.4,
-                  rectSize - rectSize * 0.8,
-                  color
-                );
-
-                if (randomise > randomiser) {
-                  let color = "black";
-
-                  droveRec(
-                    x + rectSize * 0.49,
-                    y + rectSize * 0.49,
-                    rectSize - rectSize * 0.98,
-                    color
-                  );
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+    context.fillText(text, 0, 0);
+    context.restore();
   };
 };
 
