37ab666e69155bea06351aff204f48ff480de3df
0xleodev <0xleodev@proton.me>
2023-10-03 13:31:49 +0200

    navbar in shetches

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import "./Navbar.css";
 // import { Route, Routes } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -7,12 +8,51 @@ function Navbar() {
       <div className="navbar">
         <h2 className="navbarHeader">Navigate to:</h2>
         <div className="navbarList">
-          {/* <Routes>
-            <Route path="/sketch-01" />
-            <Route path="/sketch-02" />
-            <Route path="/sketch-03" />
-          </Routes> */}
+          <NavLink className="navLink" to="/">
+            home page
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "navLink active" : "navLink"
+            }
+            to="/sketch-01"
+          >
+            sketch-01
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "navLink active" : "navLink"
+            }
+            to="/sketch-02"
+          >
+            sketch-02
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "navLink active" : "navLink"
+            }
+            to="/sketch-03"
+          >
+            sketch-03
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "navLink active" : "navLink"
+            }
+            to="/sketch-04"
+          >
+            sketch-04
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "navLink active" : "navLink"
+            }
+            to="/sketch-05"
+          >
+            sketch-05
+          </NavLink>
         </div>
+        <div>created by: 0xLeoDev</div>
       </div>
     </>
   );
